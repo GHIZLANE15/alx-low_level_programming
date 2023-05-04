1d3350c7ef134612afb13fd789e3aabebfd8c7d8
Ghizlane <ghizlane.fe15@gmail.com>
2023-05-04 05:00:30 -0700

    M

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,8 +6,7 @@
  * @dest: input value
  * @src: input value
  * @n: input value
- *
- * Return: dest
+ * Return: A pointer to the resulting string dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
