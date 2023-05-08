f067f7a19c2026d4c950e8b9ab0112f6fae6119f
Ghizlane <ghizlane.fe15@gmail.com>
2023-05-08 04:26:54 -0700

    E

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _strpbrk - Search a string for any of a set of bytes
- * @s: string
- * @accept: string to match
- * Return: Pointer to the byte in s that matches one of the bytes in accept
- * or NULL if no such byte is found
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
 
 char *_strpbrk(char *s, char *accept)
