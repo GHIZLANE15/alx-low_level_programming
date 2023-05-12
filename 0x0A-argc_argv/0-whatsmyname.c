6f59786f746b5a9db5373a4628eeabe26ebb709c
Ghizlane <ghizlane.fe15@gmail.com>
2023-05-12 07:25:35 -0700

    argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 /**
  * main - prints program name, followed by a new line
  * @argc: int
- * @argc: list
+ * @argv: list
  * Return: 0
  */
 
-int main (int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
