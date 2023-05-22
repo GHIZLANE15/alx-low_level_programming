5587e616527e2b0bdf3184d8527c51f0ceabf86d
Ghizlane <ghizlane.fe15@gmail.com>
2023-05-22 06:36:47 -0700

    S

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,10 +7,10 @@
  * @age: dog's age
  * @owner: owner's name
  *
- * *Return: void
+ * Return: void
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
