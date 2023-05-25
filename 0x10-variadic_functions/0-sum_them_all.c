3f2c0f66239d3625cf3353c85563942c71abdb05
Ghizlane <ghizlane.fe15@gmail.com>
2023-05-25 04:26:29 -0700

    V

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,19 +4,20 @@
  * sum_them_all - sums variable arguments
  * @n: the number of arguments
  * @...: the integers to sum
+ *
  * Return: the integer sum
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int b = 0, i = n;
+	int s = 0, i = n;
 	va_list ap;
 
 	if (!n)
 		return (0);
 	va_start(ap, n);
 	while (i--)
-		b += va_arg(ap, int);
+		s += va_arg(ap, int);
 	va_end(ap);
-	return (b);
+	return (s);
 }
