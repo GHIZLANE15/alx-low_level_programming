82259515b85bcf6ad595bef54875e03132184690
Ghizlane <ghizlane.fe15@gmail.com>
2023-06-01 08:13:21 -0700

    S

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+
+
+void first(void);
+
+int main(void)
+{
+	printf("Main function execution started.\n");
+	return (0);
+}
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
