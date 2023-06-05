2e3411152eadd86fd67b6c79fc7dcdf7808bd755
Ghizlane <ghizlane.fe15@gmail.com>
2023-06-05 10:39:25 -0700

    More

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,9 +16,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!head || !new_node)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
-
+	new_node->n = n;
+	new_node->next = NULL;
 	if (!*head)
 		*head = new_node;
 	else
