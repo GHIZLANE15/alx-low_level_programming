214bd502f92c2d2afcb10410352fc72be31b6d8f
Ghizlane <ghizlane.fe15@gmail.com>
2023-06-12 10:01:19 -0700

    F

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,9 +29,9 @@ int main(int ac, char **av)
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((b = read(from_fd, buf , READ_BUF_SIZE)) > 0)
-		if (write(to_fd, buf , b) != b)
-			dprintf(STDERR_FILENO, ERR_NOREAD, av[2], exit(99);
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
+		if (write(to_fd, buf, b) != b)
+			dprintf(STDERR_FILENO, ERR_NORITE, av[2], exit(99);
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
